5a3e689e7e8d416fc651402a01e3fcd0d8969a25
MaksymilianSzym <114918367+MaksymilianSzymankiewicz@users.noreply.github.com>
2022-10-03 11:19:50 +0000

    zrobiono zadanie 1-4 z karty pracy 1

---
+++ KartaPracy1.py
@@ -0,0 +1,56 @@
+#zad1
+
+
+# a = int(input())
+# b = int(input())
+# print(a**2 + b**2)
+
+#Zad2
+
+# a = int(input())
+# b = int(input())
+
+# print((a + b)**2)
+
+
+#Zad3
+
+# a = int(input())
+# b = int(input())
+
+# print((a - b)**3)
+
+#Zad4
+
+# a, b, c  = map(int, input().split())
+
+# print(a*b*c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
