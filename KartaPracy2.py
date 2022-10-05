1070d0e9d257f1e16898946a466add1910104404
MaksymilianSzym <114918367+MaksymilianSzymankiewicz@users.noreply.github.com>
2022-10-05 11:15:51 +0000

    Zadanie 1-3 karata pracy 2

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -1,3 +1,59 @@
 # Zadanie 1
 
-a = int(input())
+# a = int(input("Podaj Liczbe "))
+# if a % 3 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#Zadanie 2
+
+# a = int(input("podaj liczbę "))
+# if a>=100 and a <1000 and a % 17 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#ZADANIE 3
+
+# wiek = int(input("Ile masz lat?\n"))
+# if wiek >= 18:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
