11e5f783dafbb919a0eb633887b5eadc8c77504e
MaksymilianSzym <114918367+MaksymilianSzymankiewicz@users.noreply.github.com>
2022-10-17 11:20:40 +0000

    gggg

---
+++ KartaPracy3.py
@@ -0,0 +1,42 @@
+# # # Zad 2
+# for i in range(99,9, -1):
+  
+#    print(i, end=" " )
+
+# # Zad 3
+# for i in range(10,100, 1):
+#    print(109 - i, end=" " )
+
+
+# Zad 2
+# for i in range(105, 1000, 15): 
+#   print(i, end="  ")
+
+#PRE 2
+
+#ZAD1
+# for i in range(104, 1000, 13):
+#   print(i, end=" ")
+
+#Zad 2
+# for i in range(10, 99, 2):
+#   print(i, end=" ")
+
+#Zad 3
+
+# for i in range(0, 10):
+#   print(i**2, end=" ")
+
+
+#Zad 3
+
+# p = int(input())
+
+# for i in range(1, p+1):
+#   if p % i == 0:
+#     print(i)
+  
+
+
+
+
