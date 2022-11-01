4f90981c949a665265e8c65c16ac30e3e8fe790c
MaksymilianSzym <114918367+MaksymilianSzymankiewicz@users.noreply.github.com>
2022-11-01 15:11:25 +0000

    skonczylem kartepracy3b

---
+++ KartaPracy3a.py
@@ -0,0 +1,19 @@
+#Zad1
+
+# n = int(input())
+
+# for i in range(n):
+#   print("*-|", end="")
+
+#PRE1
+# n = int(input())
+# print("*-|" * n)
+
+#Zad2 
+# n = int(input())
+# for i in range(1, n):
+#   print("*" * i, end="")
+#   if i % 2 ==1:
+#     print("||", end="")
+#   else:
+#     print("--", end="")
